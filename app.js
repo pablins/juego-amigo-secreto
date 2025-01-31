e23b2326a799f1b21382048ae8f2ba5eb736b8f2
Pablo Rodriguez <prodriguez769@gmail.com>
2025-01-31 18:43:26 -0500

    Selecciona de manera pseudoaletaria unos de los nombres de la lista de amigos

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ let amigos = [];
 const agregarAmigo = function() {
     let inputNombre = document.getElementById("amigo");
     let nombreIngresado = inputNombre.value;
+
+    let elementoResultado = document.getElementById("resultado");
+    elementoResultado.innerHTML = "";
     
     if (nombreIngresado === "") {
         alert("Por favor, inserte un nombre.");
@@ -17,15 +20,35 @@ const agregarAmigo = function() {
 
 }
 
-const actualizarVistaListaAmigos = function () {
+const actualizarVistaListaAmigos = function() {
     let elementoListaAmigos = document.querySelector("#listaAmigos");
     //limpiamos para asegurarnos de no duplicar registros cada vez que se agregan amigos
     elementoListaAmigos.innerHTML = "";
 
-    amigos.forEach(function(name, index, arr) {
+    amigos.forEach(function(name) {
         let newItemList = document.createElement("li");
         newItemList.textContent = name;
         elementoListaAmigos.appendChild(newItemList);
         
     });
+}
+
+const sortearAmigo = function() {
+    if (amigos.length === 0) {
+        alert("Primero debes añadir nombres a la lista");
+
+    } else {
+        let nombreSorteado = amigos[generarIndiceAleatorio()];
+        let elementoResultado = document.getElementById("resultado");
+
+        elementoResultado.innerHTML = `El amigo secreto sorteado es: ${nombreSorteado}`;
+        // Limpiamos la lista
+        amigos.splice(0, amigos.length);
+        actualizarVistaListaAmigos();
+
+    }
+}
+
+const generarIndiceAleatorio = function() {
+    return Math.floor(Math.random() * amigos.length);
 }
