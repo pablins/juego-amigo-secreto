b8735583c1043f4ee9391fba9651c2a4fee83ef7
Pablo Rodriguez <prodriguez769@gmail.com>
2025-01-31 18:18:57 -0500

    Agrega nombre y renderiza la lista

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+const agregarAmigo = function() {
+    let inputNombre = document.getElementById("amigo");
+    let nombreIngresado = inputNombre.value;
+    
+    if (nombreIngresado === "") {
+        alert("Por favor, inserte un nombre.");
+        
+    } else {
+        amigos.push(nombreIngresado);
+        inputNombre.value = "";
+        actualizarVistaListaAmigos();
+
+    }
+
+}
+
+const actualizarVistaListaAmigos = function () {
+    let elementoListaAmigos = document.querySelector("#listaAmigos");
+    //limpiamos para asegurarnos de no duplicar registros cada vez que se agregan amigos
+    elementoListaAmigos.innerHTML = "";
+
+    amigos.forEach(function(name, index, arr) {
+        let newItemList = document.createElement("li");
+        newItemList.textContent = name;
+        elementoListaAmigos.appendChild(newItemList);
+        
+    });
+}
