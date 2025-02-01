909c9007c0b9d4e9ad815aec7369c843f951c6e0
Pablo Rodriguez <prodriguez769@gmail.com>
2025-01-31 19:51:37 -0500

    Restructura el código y valida no se repitan nombres

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-const agregarAmigo = function() {
-    let inputNombre = document.getElementById("amigo");
-    let nombreIngresado = inputNombre.value;
-
-    let elementoResultado = document.getElementById("resultado");
-    elementoResultado.innerHTML = "";
-    
-    if (nombreIngresado === "") {
-        alert("Por favor, inserte un nombre.");
-        
-    } else {
-        amigos.push(nombreIngresado);
-        inputNombre.value = "";
-        actualizarVistaListaAmigos();
-
-    }
-
+const asignarTextoElemento = function(selectorElemento, nuevoTexto) {
+    let elemento = document.querySelector(selectorElemento);
+    elemento.innerHTML = nuevoTexto;
 }
 
 const actualizarVistaListaAmigos = function() {
@@ -33,20 +19,70 @@ const actualizarVistaListaAmigos = function() {
     });
 }
 
-const sortearAmigo = function() {
-    if (amigos.length === 0) {
-        alert("Primero debes añadir nombres a la lista");
+const mayusculaPrimeraLetra = function(texto) {
+    return String(texto).charAt(0).toUpperCase() + String(texto).toLowerCase().slice(1);
+}
+
+const agregarAmigo = function() {
+    let inputNombre = document.getElementById("amigo");
+    let nombreIngresado = String(inputNombre.value).trim();
+
+    //limpiamos el resultado en caso que exista información
+    asignarTextoElemento("#resultado", "");
+    
+    if (nombreIngresado === "") {
+        alert("Por favor, inserte un nombre.");
+
+    } else if (amigos.includes(mayusculaPrimeraLetra(nombreIngresado))) {
+        alert(`En la lista ya tenías el nombre "${nombreIngresado}". No deberías repetir personas`);
+        inputNombre.value = "";
+
+    } else if (!isNaN(nombreIngresado)) {
+        alert(`Ingresaste el número ${nombreIngresado}. Verifica e ingresa un nombre`);
+        inputNombre.value = "";
 
     } else {
-        let nombreSorteado = amigos[generarIndiceAleatorio()];
-        let elementoResultado = document.getElementById("resultado");
-
-        elementoResultado.innerHTML = `El amigo secreto sorteado es: ${nombreSorteado}`;
-        // Limpiamos la lista
-        amigos.splice(0, amigos.length);
+        amigos.push(mayusculaPrimeraLetra(nombreIngresado));
+        inputNombre.value = "";
         actualizarVistaListaAmigos();
 
     }
+
+}
+
+const cumpleRequisitosParaSorteo = function () {
+    const MENSAJES_ERROR = {
+        LISTA_VACIA: "Primero debes añadir nombres a la lista",
+        MINIMO_NOMBRES: "Debes ingresar por lo menos 2 nombres para iniciar el sorteo"
+    };
+
+    if (amigos.length === 0) {
+        alert(MENSAJES_ERROR.LISTA_VACIA);
+        return false;
+    }
+
+    if (amigos.length < 2) {
+        alert(MENSAJES_ERROR.MINIMO_NOMBRES);
+        return false;
+    }
+
+    return true;
+}
+
+const sortearAmigo = function() {
+
+    if (!cumpleRequisitosParaSorteo()) 
+        return;
+    
+    let nombreSorteado = amigos[generarIndiceAleatorio()];
+    //Mostramos mensaje de nombre sorteado
+    asignarTextoElemento("#resultado", `El amigo secreto sorteado es: ${nombreSorteado}`);
+    
+    // Limpiamos la lista
+    amigos = [];
+    actualizarVistaListaAmigos();
+
+
 }
 
 const generarIndiceAleatorio = function() {
